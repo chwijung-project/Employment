2f80b355f68d9993f9a8d43a0434c9cad1263188
creaton60 <lovmistake@gmail.com>
2024-09-26 20:42:31 +0900

    [update] ApiErrorView 구조 생성

--- src/main/java/com/chwimong/project/employment/ui/view/ApiErrorView.java
+++ src/main/java/com/chwimong/project/employment/ui/view/ApiErrorView.java
@@ -1,10 +1,47 @@
 package com.chwimong.project.employment.ui.view;
 
+import com.chwimong.project.employment.exception.ChwimongException;
+import com.chwimong.project.employment.exception.MessageType;
 import lombok.Getter;
 import lombok.ToString;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @ToString
 public class ApiErrorView {
-    //TODO: Api Error View -> code / message /
+    private final List<Error> errors;
+
+    public ApiErrorView(List<MessageType> messageTypes) {
+        this.errors = messageTypes.stream().map(Error::errorWithMessageType).collect(Collectors.toList());
+    }
+    public ApiErrorView(MessageType messageType) {
+        this.errors = Collections.singletonList(Error.errorWithMessageType(messageType));
+    }
+
+    public ApiErrorView(ChwimongException exception) {
+        this.errors = Collections.singletonList(Error.errorWithException(exception));
+    }
+
+    @Getter
+    @ToString
+    public static class Error {
+        private final String errorType;
+        private final String errorMessage;
+
+        public static Error errorWithMessageType(MessageType messageType) {
+            return new Error(messageType.name(), messageType.getMessage());
+        }
+
+        public static Error errorWithException(ChwimongException exception) {
+            return new Error(exception.getType(), exception.getMessage());
+        }
+
+        private Error(String errorType, String errorMessage) {
+            this.errorType = errorType;
+            this.errorMessage = errorMessage;
+        }
+    }
 }
