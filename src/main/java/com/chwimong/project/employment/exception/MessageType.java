8e2115e1a254d66618f3ac0c09e2070e1351f98d
creaton60 <lovmistake@gmail.com>
2024-09-26 20:32:39 +0900

    [update] Getter Lombok Annotation 추가

--- src/main/java/com/chwimong/project/employment/exception/MessageType.java
+++ src/main/java/com/chwimong/project/employment/exception/MessageType.java
@@ -1,7 +1,9 @@
 package com.chwimong.project.employment.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public enum MessageType {
     BAD_REQUEST     ("Check API request URL protocol, parameter, etc. for errors", HttpStatus.BAD_REQUEST),
     NOT_FOUND ("No data was found for the server. Please refer to parameter description.", HttpStatus.NOT_FOUND),
