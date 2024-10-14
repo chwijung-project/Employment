14cdab9f0013ad60b6c8cfd5e9f98a51cb0c84b4
rlaeogus95 <rlaeogus95@gmail.com>
2024-10-15 02:08:27 +0900

    [update]: EmploymentController 구성

--- src/main/java/com/chwimong/project/employment/ui/controller/EmploymentController.java
+++ src/main/java/com/chwimong/project/employment/ui/controller/EmploymentController.java
@@ -1,17 +1,23 @@
 package com.chwimong.project.employment.ui.controller;
 
+import java.util.List;
 
-import com.chwimong.project.employment.ui.view.ApiResponseView;
-import com.chwimong.project.employment.ui.view.EmploymentListView;
-import com.chwimong.project.employment.ui.view.EmploymentView;
-import com.chwimong.project.employment.usecase.EmploymentFindUseCase;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.chwimong.project.config.Criteria;
+import com.chwimong.project.config.Page;
+import com.chwimong.project.employment.exception.MessageType;
+import com.chwimong.project.employment.ui.view.ApiResponseView;
+import com.chwimong.project.employment.ui.view.EmploymentListView;
+import com.chwimong.project.employment.usecase.EmploymentFindUseCase;
+import com.chwimong.project.employment.usecase.EmploymentFindUseCase.EmploymentFindQuery;
+import com.chwimong.project.employment.usecase.EmploymentFindUseCase.FindEmploymentResult;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
@@ -24,17 +30,25 @@ public class EmploymentController {
         this.employmentFindUseCase = employmentFindUseCase;
     }
 
-    @GetMapping("")
-    public ResponseEntity<ApiResponseView<EmploymentListView>> getEmployments() {
-        //TODO: input param needs
-        //TODO: service call
+    @GetMapping("/list")
+    public ResponseEntity<ApiResponseView<EmploymentListView>> getEmployments(@ModelAttribute EmploymentFindQuery query) {
 
-        return ResponseEntity.ok().build();
+    	Criteria cri = new Criteria(Integer.valueOf(query.getOffset()), 10);
+    	int total = employmentFindUseCase.getEmploymentsSize();
+    	
+    	Page pageInfo = new Page(cri, total);
+    	
+        List<FindEmploymentResult> employmentResults = employmentFindUseCase.getEmployments(cri, query);
+        EmploymentListView employmentListView = new EmploymentListView(employmentResults);
+        
+        ApiResponseView<EmploymentListView> responseView = ApiResponseView.of(MessageType.OK, employmentListView, pageInfo);
+        
+        return ResponseEntity.ok(responseView);
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<ApiResponseView<EmploymentView>> getEmployment(@PathVariable String id) {
-
-        return ResponseEntity.ok().build();
-    }
+    
+//  @GetMapping("/{id}")
+//  public ResponseEntity<ApiResponseView<EmploymentView>> getEmployment(@ModelAttribute EmploymentFindQuery query) {
+//
+//      return ResponseEntity.ok().build();
+//  }
 }
