37aade5339e04dc8cff3a70494321e4edb63f316
rlaeogus95 <rlaeogus95@gmail.com>
2024-10-15 02:09:51 +0900

    [update]: EmploymentView 구성

--- src/main/java/com/chwimong/project/employment/ui/view/EmploymentView.java
+++ src/main/java/com/chwimong/project/employment/ui/view/EmploymentView.java
@@ -1,9 +1,20 @@
 package com.chwimong.project.employment.ui.view;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
+@AllArgsConstructor
 public class EmploymentView {
+	private String recruit;
+    private String company;
+    private String region;
+    private String job;
+    private String url;
+    private String endDate;
+    private String closed;
+    private String crawlingDate;
+    private String logo;
 }
