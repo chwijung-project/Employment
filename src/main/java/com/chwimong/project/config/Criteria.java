306de4fdf0f3bab9c742ec9bbed1a88f17bd6128
rlaeogus95 <rlaeogus95@gmail.com>
2024-10-15 02:12:28 +0900

    [create]: Criteria 구성

---
+++ src/main/java/com/chwimong/project/config/Criteria.java
@@ -0,0 +1,43 @@
+package com.chwimong.project.config;
+
+import lombok.ToString;
+
+@ToString
+public class Criteria {
+    private int pageNum;
+    private int amount;
+    private String searchValue;
+
+    public Criteria(){
+        this(1, 10);
+    }
+
+    public Criteria(int pageNum, int amount) {
+        this.pageNum = pageNum;
+        this.amount = amount;
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getSearchValue() {
+        return searchValue;
+    }
+
+    public void setSearchValue(String searchValue) {
+        this.searchValue = searchValue;
+    }
+}
