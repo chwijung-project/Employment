42983b220f78a22e167786e01cc7af90d7f804a8
rlaeogus95 <rlaeogus95@gmail.com>
2024-10-15 02:09:31 +0900

    [update]: EmploymentListView 구성

--- src/main/java/com/chwimong/project/employment/ui/view/EmploymentListView.java
+++ src/main/java/com/chwimong/project/employment/ui/view/EmploymentListView.java
@@ -1,12 +1,37 @@
 package com.chwimong.project.employment.ui.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.chwimong.project.employment.usecase.EmploymentFindUseCase.FindEmploymentResult;
+
 import lombok.Getter;
 import lombok.ToString;
-
-import java.util.List;
 
 @Getter
 @ToString
 public class EmploymentListView {
-    private List<EmploymentView> employments;
+	
+    private final List<EmploymentView> employments;
+
+    public EmploymentListView(List<FindEmploymentResult> result) {
+        this.employments = result.stream()
+            .map(this::convertToEmploymentView)
+            .collect(Collectors.toList());
+    }
+
+    private EmploymentView convertToEmploymentView(FindEmploymentResult result) {
+        return new EmploymentView(
+    		result.getRecruit(),
+            result.getCompany(),
+            result.getRegion(),
+            result.getJob(),
+            result.getUrl(),
+            result.getEndDate(),
+            result.getClosed(),
+            result.getCrawlingDate(),
+            result.getLogo()
+        );
+    }
 }
+
