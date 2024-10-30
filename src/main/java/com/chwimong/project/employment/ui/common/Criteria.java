5c197e39f52e3685f7b12f3e247ce4d294a0d399
rlaeogus95 <rlaeogus95@gmail.com>
2024-10-31 01:45:59 +0900

    [update] ui 패키지로 이동

--- src/main/java/com/chwimong/project/employment/ui/common/Criteria.java
+++ src/main/java/com/chwimong/project/employment/ui/common/Criteria.java
@@ -1,7 +1,11 @@
-package com.chwimong.project.config;
+package com.chwimong.project.employment.ui.common;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+@Getter
+@Setter
 @ToString
 public class Criteria {
     private int pageNum;
@@ -15,29 +19,5 @@ public class Criteria {
     public Criteria(int pageNum, int amount) {
         this.pageNum = pageNum;
         this.amount = amount;
-    }
-
-    public int getPageNum() {
-        return pageNum;
-    }
-
-    public void setPageNum(int pageNum) {
-        this.pageNum = pageNum;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public String getSearchValue() {
-        return searchValue;
-    }
-
-    public void setSearchValue(String searchValue) {
-        this.searchValue = searchValue;
     }
 }
--- src/main/java/com/chwimong/project/employment/ui/common/Page.java
+++ src/main/java/com/chwimong/project/employment/ui/common/Page.java
@@ -1,5 +1,12 @@
-package com.chwimong.project.config;
+package com.chwimong.project.employment.ui.common;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Page {
 	
     private int pageStart;
@@ -27,58 +34,4 @@ public class Page {
         this.prev = this.pageStart > 1;
         this.next = this.pageEnd < realEnd;
     }
-
-    public int getPageStart() {
-        return pageStart;
-    }
-
-    public void setPageStart(int pageStart) {
-        this.pageStart = pageStart;
-    }
-
-    public int getPageEnd() {
-        return pageEnd;
-    }
-
-    public void setPageEnd(int pageEnd) {
-        this.pageEnd = pageEnd;
-    }
-
-    public boolean isNext() {
-        return next;
-    }
-
-    public void setNext(boolean next) {
-        this.next = next;
-    }
-
-    public boolean isPrev() {
-        return prev;
-    }
-
-    public void setPrev(boolean prev) {
-        this.prev = prev;
-    }
-
-    public int getTotal() {
-        return total;
-    }
-
-    public void setTotal(int total) {
-        this.total = total;
-    }
-
-    public Criteria getCri() {
-        return cri;
-    }
-
-    public void setCri(Criteria cri) {
-        this.cri = cri;
-    }
-
-	@Override
-	public String toString() {
-		return "Page [pageStart=" + pageStart + ", pageEnd=" + pageEnd + ", next=" + next + ", prev=" + prev
-				+ ", total=" + total + ", cri=" + cri + "]";
-	}
 }
