59d402ee53f6ad8339d3982f847b63255f27f982
unknown <rlaeogus95@gmail.com>
2024-12-23 23:08:18 +0900

    [create] useCase / inner class 생성

---
+++ src/main/java/com/chwimong/project/employment/ui/view/FilterResponse.java
@@ -0,0 +1,11 @@
+package com.chwimong.project.employment.ui.view;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FilterResponse {
+	private final int job_count;
+	private final int total_count;
+}
