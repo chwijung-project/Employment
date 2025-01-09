67d9aae7f91abc3cdd4281633e0ca39b993e0241
creaton60 <lovmistake@gmail.com>
2025-01-09 22:56:37 +0900

    [create] EmploymentSummary for Filter System View Model

---
+++ src/main/java/com/chwimong/project/employment/ui/view/EmploymentSummaryListView.java
@@ -0,0 +1,12 @@
+package com.chwimong.project.employment.ui.view;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@ToString
+public class EmploymentSummaryListView {
+    private List<EmploymentSummaryView> summaryViewList;
+}
