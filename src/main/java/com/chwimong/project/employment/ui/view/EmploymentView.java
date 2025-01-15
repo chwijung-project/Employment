6a62a156007dc2e168370243491ac323b7860154
unknown <rlaeogus95@gmail.com>
2025-01-15 11:13:19 +0900

    [update] 형식 수정

--- src/main/java/com/chwimong/project/employment/ui/view/EmploymentView.java
+++ src/main/java/com/chwimong/project/employment/ui/view/EmploymentView.java
@@ -1,5 +1,7 @@
 package com.chwimong.project.employment.ui.view;
 
+import com.chwimong.project.employment.usecase.EmploymentFindUseCase;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,7 +15,19 @@ public class EmploymentView {
     private String job;
     private String url;
     private String endDate;
-    private String closed;
+    private Boolean closed;
     private String crawlingDate;
     private String logo;
+
+    public EmploymentView(EmploymentFindUseCase.FindEmploymentResult result) {
+    	this.recruit = result.getRecruit();
+        this.company = result.getCompany();
+        this.region = result.getRegion();
+        this.job = result.getJob();
+        this.url = result.getUrl();
+        this.endDate = result.getEndDate();
+        this.closed = result.getClosed();
+        this.crawlingDate = result.getCrawlingDate();
+        this.logo = result.getLogo();
+    }
 }
