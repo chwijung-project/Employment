4fce79c8a347142f3f41f1d6a1a7b010e7b4887b
unknown <rlaeogus95@gmail.com>
2025-01-21 23:21:44 +0900

    [create] swagger ui config 생성

---
+++ src/main/java/com/chwimong/project/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.chwimong.project.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Chwimong API",
+		description = "API Documentation for Chwimong Service",
+		version = "1.0.0"
+	),
+	servers = {
+        @Server(
+            description = "개발 서버",
+            url = "http://localhost:8080"
+        ),
+        @Server(
+            description = "운영 서버",
+            url = "http://www.chwimong.com"
+        )
+    }
+	
+)
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+	@Bean
+	GroupedOpenApi employmentApi() {
+	    return GroupedOpenApi.builder()
+	            .group("채용정보 API")
+	            .pathsToMatch("/v1/employments/**")
+	            .build();
+	}
+
+	@Bean
+	GroupedOpenApi compactEmploymentApi() {
+	    return GroupedOpenApi.builder()
+	            .group("요약 채용정보 API")
+	            .pathsToMatch("/v1/view/compact-employments/**")
+	            .build();
+	}
+}
