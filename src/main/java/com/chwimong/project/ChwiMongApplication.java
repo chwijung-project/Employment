6096f19404d7e0b4ab4edbbefb4877c5af7ae4c5
unknown <rlaeogus95@gmail.com>
2025-02-06 21:32:43 +0900

    [etc]

--- src/main/java/com/chwimong/project/ChwiMongApplication.java
+++ src/main/java/com/chwimong/project/ChwiMongApplication.java
@@ -3,11 +3,8 @@ package com.chwimong.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.chwimong.project")
