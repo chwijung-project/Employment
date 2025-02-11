a3c932c3c740966e638bc8d2ac8b34c6f30067b7
unknown <rlaeogus95@gmail.com>
2025-02-11 22:31:15 +0900

    [update] searchValue 필드 이동

--- src/main/java/com/chwimong/project/employment/ui/request/FindEmploymentRequest.java
+++ src/main/java/com/chwimong/project/employment/ui/request/FindEmploymentRequest.java
@@ -3,9 +3,11 @@ package com.chwimong.project.employment.ui.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 public class FindEmploymentRequest {
 
@@ -13,8 +15,8 @@ public class FindEmploymentRequest {
     private String job;
     private String region;
     private String sort;
-    private Boolean closed;
     private String offset;
+    private String searchValue;
     
     public String getOffset() {
         return (offset == null || offset.isEmpty()) ? "1" : offset;
