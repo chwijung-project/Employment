1081d07a56a8459c9617e43ece6d0a5b65c54eb3
kdh <rlaeogus95@gmail.com>
2025-04-17 21:26:52 +0900

    [create]: Commercial 뷰 클래스 추가

---
+++ src/main/java/com/chwimong/project/education/ui/view/commercial/CommercialListView.java
@@ -0,0 +1,24 @@
+package com.chwimong.project.education.ui.view.commercial;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.chwimong.project.education.usecase.CommercialFindUseCase.FindCommercialResult;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class CommercialListView {
+	
+	@JsonValue
+	private List<CommercialView> commercialViewList;
+	
+	public CommercialListView(List<FindCommercialResult> result) {
+		this.commercialViewList = result.stream()
+			.map(CommercialView::new)
+			.collect(Collectors.toList());
+	}
+}
